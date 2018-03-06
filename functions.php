d325915f6d7235014b525cd26a3af5c1ea5be5c8
Takenori Okashita (GOUTEN) <gouten@5010works.com>
2018-03-06 13:50:16 +0900

    Update.

--- functions.php
+++ functions.php
@@ -8,9 +8,9 @@
 /**
  * Add stylesheets.
  */
-function wordcamp_japan_theme_style() {
+function wordcamp_site_theme_style() {
 	wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
-	wp_enqueue_style( 'wordcamp-japan-theme-style', get_stylesheet_directory_uri() . '/css/wordcamp-japan-theme.css', array( 'parent-style' ) );
+	wp_enqueue_style( 'wordcamp-site-theme-style', get_stylesheet_directory_uri() . '/css/wcosaka2018.css', array( 'parent-style' ) );
 }
 
-add_action( 'wp_enqueue_scripts', 'wordcamp_japan_theme_style' );
+add_action( 'wp_enqueue_scripts', 'wordcamp_site_theme_style' );
